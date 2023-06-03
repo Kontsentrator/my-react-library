dabab3b86e0d16eb5e5d90d8cc09f351ba3ad942
Kontsentrator <87811683+Kontsentrator@users.noreply.github.com>
2023-06-03 07:55:04 +0000

    Deploying to gh-pages from @ Kontsentrator/my-react-library@fb821b931d087dec5330886d7e3315fb9f229ddf 🚀

--- src/components/Button/config.ts
+++
@@ -1,5 +0,0 @@
-const ButtonStatus = ['system', 'success', 'error'] as const;
-export type ButtonStatusType = typeof ButtonStatus[number];
-
-const ButtonView = ['primary', 'secondary'] as const;
-export type ButtonViewType = typeof ButtonView[number];
